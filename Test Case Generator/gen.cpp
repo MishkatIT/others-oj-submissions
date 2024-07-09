b76161ecdc719eb08b92289a002a2a5c21f035c4
Mishkatul Islam <mishkatcse1@gmail.com>
2024-07-09 19:23:46 +0600

    gen updated

--- Test Case Generator/gen.cpp
+++ Test Case Generator/gen.cpp
@@ -14,26 +14,27 @@ mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
 void generate(int currentNum) {
     string folder = "testcases/";
     ofstream inFile(folder + to_string(currentNum) + ".in");
-    // ofstream ansFile(folder + to_string(currentNum) + ".ans");
-    // ofstream outFile(folder + to_string(currentNum) + ".out");
 
+
+
+    //modify from here
     int n = N;
     vector<int> v(n);
     for (int i = 0; i < n; i++) {
-        v[i] = rnd() % 1000; // generate random numbers in range [0, 999]
+        v[i] = rnd() % 1000; 
         inFile << v[i] << " ";
     }
     inFile << '\n';
+    // end of modifications
+
 
     inFile.close();
-    // ansFile.close();
-    // outFile.close();
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int testCase = rnd() % 15 + 1;
+    int testCase = 15;
     for (int t = 0; t < testCase; t++) {
         generate(t + 10);
     }
