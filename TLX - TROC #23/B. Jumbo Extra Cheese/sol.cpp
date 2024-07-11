244d41ba927e267c426f633c7a4836e49926e548
Mishkatul Islam <mishkatcse1@gmail.com>
2024-07-12 00:58:02 +0600

    .

--- TLX - TROC #23/B. Jumbo Extra Cheese/sol.cpp
+++ TLX - TROC #23/B. Jumbo Extra Cheese/sol.cpp
@@ -1,6 +1,5 @@
 /*
     Author    : MishkatIT
-    Created   : Monday 08-07-2024 15:59:49
 */
 
 #include <bits/stdc++.h>
@@ -19,16 +18,59 @@ const ll N = 2e5 + 10;
 const ll inf = 1e9;
 const ll linf = 1e18;
 
+vector<int> parent, rank_;
+
+int find(int x) {
+    if (parent[x] != x) {
+        parent[x] = find(parent[x]); // Path compression
+    }
+    return parent[x];
+}
+
+void unite(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+
+    if (rootX != rootY) {
+        if (rank_[rootX] > rank_[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank_[rootX] < rank_[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+            rank_[rootX]++;
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin >> n;
-    int ans = 0;
-    for (int i = 19; i <= min(99, n); i += 10) {
-        ans++;
+    int n, q;
+    cin >> n >> q;
+
+    parent.resize(n + 1);
+    rank_.resize(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+        parent[i] = i;
     }
-    cout << ans << '\n';
+
+    while (q--) {
+        int s, t;
+        cin >> s >> t;
+        unite(s, t);
+    }
+
+    vector<int> result(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        result[i] = find(i);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        cout << result[i] << ' ';
+    }
+    cout << '\n';
+
     return 0;
 }
