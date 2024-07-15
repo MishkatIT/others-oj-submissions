4e586398ce6ed832cb0c12b11bc4d2b33738ff2f
Mishkatul Islam <mishkatcse1@gmail.com>
2024-07-15 23:53:03 +0600

    .

---
+++ AtCoder - Toyota Programming Contest 2024#7（AtCoder Beginner Contest 362）/C - Sum = 0/sol.cpp
@@ -0,0 +1,39 @@
+/*
+    Author    : MishkatIT
+    Created   : Monday 15-07-2024 20:29:08
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+using ll = long long;
+using ld = long double;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 10;
+const int inf = 1e9;
+const ll linf = 1e18;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        ll l, r;
+        cin >> l >> r;
+        ll ans;
+        int i = 0;
+        ll cur;
+        while (true) {
+            
+        }
+    }
+    return 0;
+}
